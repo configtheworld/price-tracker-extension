7f11ac9fd65e920207f6306536ab163cc888d234
configtheworld <erkinture@gmail.com>
2022-03-07 00:40:35 +0300

    url listener added

--- popout.js
+++ popout.js
@@ -1,3 +1,4 @@
+// Initial queries
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   var activeTab = tabs[0];
   var obj = {};
@@ -5,11 +6,26 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   chrome.tabs.sendMessage(activeTab.id, { command: 'runCommands', data: obj });
 });
 
+// controllers
+
 // get the tab's url
-function getCurrentTabsUrl() {}
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true,
+  };
+
+  chrome.tabs.query(queryInfo, function (tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    callback(url);
+  });
+}
 
 // validate current page is product in selected websides
-function validateUrl(url) {}
+function validateUrl(url) {
+  console.log(url);
+}
 
 // decide the type of the website
 function decideType(url) {}
@@ -22,3 +38,11 @@ function addProductList(obj) {}
 
 // remove item
 function removeProductList(obj) {}
+
+// event listeners
+
+document.querySelector('.get-url').addEventListener('click', function () {
+  getCurrentTabUrl(function (url) {
+    validateUrl(url);
+  });
+});
